ba3f2232e416127a05f539b79593ef988af19c65
BugsCleaners <bugscleaners@gmail.com>
2022-04-25 14:35:45 +0300

    added

---
+++ Tutorial 6/Tutorial6/src/test/java/com/Tutorial6/Tutorial6/Tutorial6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tutorial6.Tutorial6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
